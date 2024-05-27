360673dc445caa7b1a934b449aaf140a546dbb4a
devmohamedgamal <mpedo2010@gmail.com>
2024-05-27 17:18:36 +0300

    add assets & text styles

---
+++ lib/utils/assets_manger.dart
@@ -0,0 +1,18 @@
+class AssetsManger {
+  AssetsManger._();
+  static const String imagesBalance = "assets/images/balance.svg";
+  static const String imagesCardBackgeound = "assets/images/card_backgeound.svg";
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+  static const String imagesExpenses = "assets/images/expenses.svg";
+  static const String imagesGallery = "assets/images/gallery.svg";
+  static const String imagesIncome = "assets/images/income.svg";
+  static const String imagesInvestment = "assets/images/investment.svg";
+  static const String imagesLogout = "assets/images/logout.svg";
+  static const String imagesProfileIcon = "assets/images/profile_icon.svg";
+  static const String imagesProfileIcon2 = "assets/images/profile_icon2.svg";
+  static const String imagesSetting = "assets/images/setting.svg";
+  static const String imagesStatiscs = "assets/images/statiscs.svg";
+  static const String imagesTransactions = "assets/images/transactions.svg";
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
+
